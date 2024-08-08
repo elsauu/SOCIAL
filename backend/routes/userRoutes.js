8c799635f6aaf1676201e0b1d94b6043c2f05369
Esau Garcia <esautfu@gmail.com>
2024-08-08 03:00:16 -0500

    user route update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
-	followUnFollowUser,
-	getUserProfile,
-	loginUser,
-	logoutUser,
-	signupUser,
-	updateUser,
-	getSuggestedUsers,
-	freezeAccount,
+    followUnFollowUser,
+    getUserProfile,
+    loginUser,
+    logoutUser,
+    signupUser,
+    updateUser,
+    getSuggestedUsers,
+    freezeAccount,
+    updateUserTags, // Nueva función para actualizar etiquetas
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
@@ -21,5 +22,6 @@ router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
 router.put("/update/:id", protectRoute, updateUser);
 router.put("/freeze", protectRoute, freezeAccount);
+router.put("/updateTags/:id", protectRoute, updateUserTags); // Nueva ruta para actualizar etiquetas
 
 export default router;
